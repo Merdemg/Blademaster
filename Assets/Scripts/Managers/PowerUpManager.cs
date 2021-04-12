d6c4ced23cbea3657ec31e61d3ecf6f1b81cb66c
Erdem Gunay <merdemgunay88@gmail.com>
2021-04-12 19:19:36 -0400

    Visual feedback for power up duration

--- Assets/Scripts/Managers/PowerUpManager.cs
+++ Assets/Scripts/Managers/PowerUpManager.cs
@@ -5,14 +5,16 @@ using UnityEngine;
 public class PowerUpManager : MonoBehaviour
 {
     static PowerUpManager instance;
-
     public static PowerUpManager Instance {
         get { if (instance == null) instance = FindObjectOfType<PowerUpManager>(); return instance; }
     }
 
-
     [SerializeField] List<PowerUp> powerUps = new List<PowerUp>();
     PowerUp activePowerUp = null;
+
+    const float POWER_UP_DURATION = 6f;
+
+    Coroutine powerUpCoRoutine = null;
 
     public PowerUp GetRandomPowerUp() {
         return powerUps[Random.Range(0, powerUps.Count)];
@@ -21,11 +23,34 @@ public class PowerUpManager : MonoBehaviour
     public void ActivatePowerUp(PowerUp PowerUp) {
         Debug.Log("Activating power up");
 
+        DeactivatePowerUp();
+
+        activePowerUp = PowerUp;
+        PowerUp.Activate();
+        PlayerCanvas.Instance.SetImage(PowerUp.powerUpIcon);
+        powerUpCoRoutine = StartCoroutine(PowerUpCoroutine());
+    }
+
+    void DeactivatePowerUp() {
         if (activePowerUp != null) {
             activePowerUp.Deactivate();
         }
 
-        activePowerUp = PowerUp;
-        PowerUp.Activate();
+        if (powerUpCoRoutine != null) {
+            StopCoroutine(powerUpCoRoutine);
+            powerUpCoRoutine = null;
+        }
+    }
+
+    IEnumerator PowerUpCoroutine() {
+        float counter = 0;
+
+        while (counter < POWER_UP_DURATION) {
+            counter += Time.deltaTime;
+            PlayerCanvas.Instance.SetImagePerc((POWER_UP_DURATION - counter) / POWER_UP_DURATION);
+            yield return new WaitForEndOfFrame();
+        }
+
+        DeactivatePowerUp();
     }
 }
---
+++ Assets/Scripts/PlayerCanvas.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerCanvas : MonoBehaviour
+{
+    static PlayerCanvas instance;
+
+    public static PlayerCanvas Instance {
+        get { if (instance == null) instance = FindObjectOfType<PlayerCanvas>(); return instance; }
+    }
+
+    [SerializeField] Canvas canvas;
+    [SerializeField] Image image;
+
+
+
+    public void SetImage(Sprite Sprite) {
+        image.sprite = Sprite;
+        image.fillAmount = 1f;
+        canvas.enabled = true;
+    }
+
+    public void SetImagePerc(float Perc) {
+        image.fillAmount = Perc;
+    }
+
+    public void DisableImage() {
+        canvas.enabled = false;
+    }
+}
