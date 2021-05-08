4076736a3ebac7e5e2b3d59c3166258f5ebc4cb8
Erdem Gunay <merdemgunay88@gmail.com>
2021-05-08 17:58:42 -0400

    Phase power up

---
+++ Assets/Scripts/PlayerColliderController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerColliderController : MonoBehaviour
+{
+    static PlayerColliderController instance;
+
+    public static PlayerColliderController Instance { get { if (instance == null) instance = FindObjectOfType<PlayerColliderController>(); return instance; } }
+    
+
+    [SerializeField] Collider wallCollider = null;
+
+
+    public void SetWallCollider(bool IsEnabled){
+        wallCollider.enabled = IsEnabled;
+    }
+}
---
+++ Assets/Scripts/PowerUps/Phase.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "PUP_Phase", menuName = "ScriptableObjects/PowerUpPhase", order = 3)]
+public class Phase : PowerUp
+{
+    public override void Activate() {
+        base.Activate();
+        Debug.Log("Phase activated");
+        PlayerColliderController.Instance.SetWallCollider(false);
+    }
+
+    public override void Deactivate() {
+        base.Deactivate();
+        PlayerColliderController.Instance.SetWallCollider(true);
+    }
+}
